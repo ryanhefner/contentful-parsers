71965434eecb1bdf2e6e84203999aa844cabc292
Ryan Hefner <hi@ryanhefner.com>
2020-08-12 18:02:32 -0400

    Add graphqlParser tests

---
+++ src/parsers/graphqlParser.test.js
@@ -0,0 +1,104 @@
+import graphqlParser from './graphqlParser';
+
+let data;
+
+describe('graphqlParser', () => {
+  test('collection data pasered', () => {
+    data = graphqlParser('itemCollection', {
+      items: [
+        {
+          sys: {
+            id: 'testId',
+            contentType: {
+              sys: {
+                id: 'testType'
+              },
+            },
+          },
+          fields: {
+            test: 'test',
+          },
+        },
+        {
+          sys: {
+            id: 'testId',
+            contentType: {
+              sys: {
+                id: 'testType'
+              },
+            },
+          },
+          fields: {
+            test: 'test',
+          },
+        },
+      ],
+    });
+
+    expect(data).toEqual({
+      itemCollection: {
+        __typename: 'Array',
+        items: [
+          {
+            sys: {
+              __typename: 'Sys',
+              id: 'testId',
+              contentType: {
+                sys: {
+                  id: 'testType'
+                },
+              },
+            },
+            __typename: 'testType',
+            test: 'test',
+          },
+          {
+            sys: {
+              __typename: 'Sys',
+              id: 'testId',
+              contentType: {
+                sys: {
+                  id: 'testType'
+                },
+              },
+            },
+            __typename: 'testType',
+            test: 'test',
+          }
+        ]
+      }
+    });
+  });
+
+  test('entry data parsed', () => {
+    data = graphqlParser('item', {
+      sys: {
+        id: 'testId',
+        contentType: {
+          sys: {
+            id: 'testType'
+          },
+        },
+      },
+      fields: {
+        test: 'test',
+      },
+    });
+
+    expect(data).toEqual({
+      item: {
+        sys: {
+          __typename: 'Sys',
+          id: 'testId',
+          contentType: {
+            sys: {
+              id: 'testType'
+            },
+          },
+        },
+        __typename: 'testType',
+        test: 'test',
+      }
+    });
+  });
+});
